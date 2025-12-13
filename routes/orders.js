d875fa805311487f676a4f3deaa6be735dec99a7
Yehe Yan <yanyehe@Gmail.com>
2025-12-13 16:35:53 -0500

    fix refund operation in orderController

--- routes/orders.js
+++ routes/orders.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 const { verifyToken } = require('../middleware/auth');
 
+// 小程序更新退款状态
+router.put('/refunds/:refundId/status', orderController.updateRefundStatus);
 router.get('/refunds/pending', orderController.getPendingRefunds);
 router.post('/refunds/:refundId/process', orderController.processRefund);
 router.post('/refunds/manual', orderController.createManualRefund);
